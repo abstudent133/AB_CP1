28c5e85488023650ebdf1f38896a979612d95409
abstudent133 <anna.bangerter@ucas-edu.net>
2025-08-20 09:46:33 -0600

    hello_world assignment

---
+++ notes/inputs.py
@@ -0,0 +1,9 @@
+# AB 1st Inputs and Outputs notes
+name = input("what is your name: ") 
+#save input where it can be accessed again
+# Use keord "input"
+#inside parenthases give user instructions
+print("what is your name?")
+name = input()
+# this is just another way to do the same thing
+print("Hello",name)
