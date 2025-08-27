7901681c678a6fb5b27cd84dff024e2c3736e1fb
abstudent133 <anna.bangerter@ucas-edu.net>
2025-08-27 09:52:52 -0600

    Notes

---
+++ notes/integers_float.py
@@ -0,0 +1,33 @@
+#AB 1st integers and floats
+
+#integers
+year = 11
+room = 200
+
+#Float
+pi = 3.14
+
+#Math
+print(5+2)
+print(5-2)
+print(5*2)
+print(5/2)
+print(5//2)
+print(5%2)
+print(5**2)
+
+print(5+2 *9 + (5**5)%3)
+
+#type casting
+num = int(3.14)
+dec = float(8)
+
+#Using variables
+hours = 3
+minutes = 4
+#To change a user input data typ you have to surround
+print(hours*60+minutes)
+
+age = int(input("How old are you:"))
+
+print("Wow tiy are old, that is", 525600*age,"minutes!" )
--- projects/who_are_you.py
+++ projects/who_are_you.py
@@ -3,3 +3,4 @@ name = input("What is your name: ")
 age = input("How old are you: ")
 color = input("What is your favorite color: ")
 print("Wow",name,"it must be so cool being",age,"and I love",color,"too!")
+
