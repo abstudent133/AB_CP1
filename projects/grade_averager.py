1f2fe27127b9eaf936e3cbbff5c225aa0598d8d6
abstudent133 <anna.bangerter@ucas-edu.net>
2025-08-28 09:54:17 -0600

    grade averager project

---
+++ projects/grade_averager.py
@@ -0,0 +1,19 @@
+#AB 1st Average grade
+print("Hello, this is a grade averager so please enter your grade for each class in the following sections.")
+
+class_one = float(input("Class 1 grade: "))
+class_two = float(input("Class 2 grade: "))
+class_three = float(input("Class 3 grade: "))
+class_four = float(input("Class 4 grade: "))
+class_five = float(input("Class 5 grade: "))
+class_six = float(input("Class 6 grade: "))
+class_seven = float(input("Class 7 grade: "))
+
+average = round(((class_one + class_two + class_three + class_four + class_five + class_seven)/7),2)
+if average >= 92.00:
+    print("Your average is", average,"so you have an A average.")
+
+
+
+
+
---
+++ starters/starters/tax.py
@@ -0,0 +1,2 @@
+#AB CS 1st tax starter
+
