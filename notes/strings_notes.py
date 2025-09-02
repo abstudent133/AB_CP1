01d0cdecbb7476a34bafe7f02b3e3be3a3a71965
abstudent133 <anna.bangerter@ucas-edu.net>
2025-09-02 09:53:00 -0600

    strings notes

---
+++ notes/strings_notes.py
@@ -0,0 +1,17 @@
+#AB 1st string notes
+#string examples
+"The quick brown fox jumps over the lazy dog"
+#needs to look like this
+variable = "The quick brown fox jumps over the lazy dog"
+
+first_name = "Tia" 
+month = "September"
+
+len(first_name)
+#escape character
+print("\"inkheart\" is the best book")
+
+#concatnate
+added_strings = first_name + month
+print(added_strings)
+#index 
