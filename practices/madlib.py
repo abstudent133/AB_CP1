2a4383f1c6b2e7cb824e15ea2e6cc2d3a76de2b4
abstudent133 <anna.bangerter@ucas-edu.net>
2025-09-03 09:53:29 -0600

    madlib

---
+++ practices/madlib.py
@@ -0,0 +1,24 @@
+#AB 1st Madlib
+
+person_in_room = input("Put the name of a person in the room:")
+first_number = input("Give me a number:")
+adjective = input("Give me an adjective:")
+color = input('Give me a color:')
+noun_one = input('Give me a noun:')
+type_food = input('Give me a type of food:')
+noun_two = input('Give me a noun:')
+verb_ing = input('Give me a verb ending in "ing":')
+clothing = input ('Give me an article of clothing:')
+adjective_one = input("Give me an adjective:")
+celebrity = input('Give me a celebrity:')
+number_two = input("Give me a number:")
+person_in_room_two = input("Put the name of a person in the room:")
+noun_three = input('Give me a noun:')
+pir_three = input("Put the name of a person in the room:")
+occupation = input('Give me an occupation:')
+
+madlib = "My name is " + person_in_room + " and I am " + first_number + ". If I were president, I'd do a whole bunch of " + adjective + " things: 1. I would drive the biggest " + color + " car in the country. And that car would go faster than any other " + noun_one + " in the world! 2. Everyone would eat pepperoni" + type_food + " for dinner. 3. I would live in the Statue of " + noun_two + " and build a " + verb_ing + " pool at her feet. 4. i would wear a/an " + clothing + " on my head, and everyone would say I look " + adjective_one + " like " + celebrity +'. 5. School would be open only ' + number_two + ' days a year. 6. I would give my friends the best jobs. I would nominate ' + person_in_room_two + " to be secretary of (the) " + noun_three
++ ", and " + pir_three + ' could be vice ' + occupation + "!"
+
+print(madlib)
+
