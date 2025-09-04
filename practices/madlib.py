ef5cce102834aefd9a95aee7459334b26c4133d7
abstudent133 <anna.bangerter@ucas-edu.net>
2025-09-04 00:30:05 +0000

    debug madlib

--- practices/madlib.py
+++ practices/madlib.py
@@ -17,8 +17,8 @@ noun_three = input('Give me a noun:')
 pir_three = input("Put the name of a person in the room:")
 occupation = input('Give me an occupation:')
 
-madlib = "My name is " + person_in_room + " and I am " + first_number + ". If I were president, I'd do a whole bunch of " + adjective + " things: 1. I would drive the biggest " + color + " car in the country. And that car would go faster than any other " + noun_one + " in the world! 2. Everyone would eat pepperoni" + type_food + " for dinner. 3. I would live in the Statue of " + noun_two + " and build a " + verb_ing + " pool at her feet. 4. i would wear a/an " + clothing + " on my head, and everyone would say I look " + adjective_one + " like " + celebrity +'. 5. School would be open only ' + number_two + ' days a year. 6. I would give my friends the best jobs. I would nominate ' + person_in_room_two + " to be secretary of (the) " + noun_three
-+ ", and " + pir_three + ' could be vice ' + occupation + "!"
+madlib = ("My name is " + person_in_room + " and I am " + first_number + ". If I were president, I'd do a whole bunch of " + adjective + " things: 1. I would drive the biggest " + color + " car in the country. And that car would go faster than any other " + noun_one + " in the world! 2. Everyone would eat pepperoni " + type_food + " for dinner. 3. I would live in the Statue of " + noun_two + " and build a " + verb_ing + " pool at her feet. 4. I would wear a/an " + clothing + " on my head, and everyone would say I look " + adjective_one + " like " + celebrity +'. 5. School would be open only ' + number_two + ' days a year. 6. I would give my friends the best jobs. I would nominate ' + person_in_room_two + " to be secretary of (the) " + noun_three
++ ", and " + pir_three + ' could be vice ' + occupation + "!")
 
 print(madlib)
 
