35d239c17760e9726ac4e64bc6f2419646ae4fc5
abstudent133 <anna.bangerter@ucas-edu.net>
2025-09-06 01:15:16 +0000

    finish idiot proof

--- projects/idiot_proof.py
+++ projects/idiot_proof.py
@@ -1,13 +1,21 @@
 #AB 1st Idiot Proof
 
 username = input("What is your username?")
-phone_num = input('What is your phone number(add a space bettween the first three numbers, next three numbers, and the last four numbers)?')
+phone_num = input('What is your phone number?')
 gpa = float(input('What is your gpa?'))
 
 first_name, last_name = username.split('.')
 full_name = (first_name + " " + last_name).title()
+print(full_name)
+
+phone_num_1 = phone_num[:3]
+phone_num_2 = phone_num[3:6]
+phone_num_3 = phone_num[6:]
+
+print(phone_num_1 + " " + phone_num_2 + " " + phone_num_3)
 
 gpa = round(gpa,1)
+print(gpa)
 
 
 
