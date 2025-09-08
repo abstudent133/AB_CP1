79c76c628841f54c06f20cbb1ae0f0f730529f5b
abstudent133 <anna.bangerter@ucas-edu.net>
2025-09-08 09:53:18 -0600

    basic calculator

---
+++ projects/basic_calculator.py
@@ -0,0 +1,18 @@
+#AB 1st basic calculator
+
+num1 = input("Give me one number:")
+num2 = input("Give me another number:")
+
+addition = (num1+num2)
+subtraction = (num1-num2)
+multiplication = (num1*num2)
+division = (num1/num2)
+floor_divison = (num1//num2)
+modulo = (num1%num2)
+exoponent = (num1**num2)
+
+
+
+print(f"These are the answers to equations between the numbers {num1} and {num2}")
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
