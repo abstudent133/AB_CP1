cf7826948dc6ba26771a0a995abfa670b1fa642a
abstudent133 <anna.bangerter@ucas-edu.net>
2025-09-10 09:49:26 -0600

    random numbers notes and crew shares

---
+++ notes/random_nums.py
@@ -0,0 +1,13 @@
+#AB 1st random notes
+
+import random
+print(random.random( ))
+print(random.randint(1,20))
+
+stat_one = random.randint(1,10) + random.randint(1,10)
+stat_two = random.randint(1,10) + random.randint(1,10)
+stat_three = random.randint(1,10) + random.randint(1,10)
+stat_four = random.randint(1,10) + random.randint(1,10)
+stat_five = random.randint(1,10) + random.randint(1,10)
+stat_six = random.randint(1,10) + random.randint(1,10)
+stat_seven  = random.randint(1,10) + random.randint(1,10)
---
+++ projects/crew_shares.py
@@ -0,0 +1,5 @@
+#AB 1st Crew Shares
+
+money = input("What was the amout of money earned by the pirates:")
+num_crew = input("How many crew members are there:")
+
