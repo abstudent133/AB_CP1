1bc78c0bde69e512c1d392543b042b6026189e23
abstudent133 <anna.bangerter@ucas-edu.net>
2025-09-15 09:37:36 -0600

    crew shares 2.0

--- projects/crew_shares.py
+++ projects/crew_shares.py
@@ -1,5 +1,12 @@
 #AB 1st Crew Shares
 
-money = input("What was the amout of money earned by the pirates:")
-num_crew = input("How many crew members are there:")
+money = int(input("What was the amout of money earned by the pirates:"))
+num_crew = int(input("How many crew members are there:"))
+share = money/(num_crew + 10)
+crew_share = share - 500
+first_mate_share = share * 3
+captain_share = share * 7
+print(f"The captain gets: ${captain_share:.2f}")
+print(f"The first mate gets: ${first_mate_share:.2f}")
+print(f"Each member of the crew still needs: ${crew_share:.2f}")
 
