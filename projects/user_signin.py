8d755ed60f5e45ddc5aba26c3c58c9ec2f67fedc
abstudent133 <anna.bangerter@ucas-edu.net>
2025-09-22 09:32:27 -0600

    user sign in

---
+++ projects/user_signin.py
@@ -0,0 +1,17 @@
+#AB 1st User Sign In
+username = input("Input username: ")
+password = input("Input password: ")
+
+
+if username == "anna.bangerter":
+    if password == "lemonyellow":
+        print(f"Welcome, Anna Bangerter:)")
+    else:
+        print("The password is incorrect. Access Denied!")
+
+else:
+    print("The username is incorrect. Access Denied!")
+
+
+
+        
