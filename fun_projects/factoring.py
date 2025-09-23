00881463e1381980182eb2153492a117eebd1f76
abstudent133 <anna.bangerter@ucas-edu.net>
2025-09-23 09:15:15 -0600

    work on factoring project

--- fun_projects/factoring.py
+++ fun_projects/factoring.py
@@ -1,5 +1,6 @@
-number = input("Give me a number you would like to factor:")
+number = int(input("Give me a number you would like to factor:"))
 factors = {
-    1: ["1,1"],2: ["1x2"],3:["1x3"],4:["1x4,2x2"],5:['1x5'],6:['1x6,2x3'],7:[]
+    1: ["1x1"],2: ["1x2"],3:["1x3"],4:["1x4,2x2"],5:['1x5'],6:['1x6,2x3'],7:['1x7']
 
 }
+print(factors[number])
