c06b6e26b598d626944941489eec5abff224fe4e
abstudent133 <anna.bangerter@ucas-edu.net>
2025-09-25 09:52:54 -0600

    debugging notes

--- fun_projects/factoring.py
+++ fun_projects/factoring.py
@@ -1,20 +1,14 @@
 number = int(input("Give me a number you would like to factor:"))
 
 if number % 10 == 0:
-    if number % 9 == 0:
-        pass
-    elif number % 8 == 0:
+   pass
+elif number % 9 == 0:
+    if number % 8 == 0:
         pass
     elif number % 7 == 0:
         pass
     elif number % 6 == 0:
         pass
-    elif number % 4 == 0:
-        pass
-    elif number % 3 == 0:
-        pass
-    else:
-elif number % 9 == 0:
-    if number % 8
+
     
     
---
+++ notes/debugging.py
@@ -0,0 +1,14 @@
+#AB 1st Debugging notes
+
+#Syntax Error
+   #print"anna"
+#read to fix
+
+#logic error
+
+numone = "2"
+numtwo = "5"
+
+print(numone + numtwo)
+
+#run-time error
