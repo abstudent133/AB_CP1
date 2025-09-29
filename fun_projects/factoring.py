a7b76c361b3ec380430fcd1fd53d558a1b6b74ba
abstudent133 <anna.bangerter@ucas-edu.net>
2025-09-29 09:53:02 -0600

    factoring

--- fun_projects/factoring.py
+++ fun_projects/factoring.py
@@ -1,9 +1,41 @@
 number = int(input("Give me a number you would like to factor:"))
 
 if number % 10 == 0:
-   pass
+    if number % 9 == 0:
+        pass
+    elif number % 8 == 0:
+        pass
+    elif number % 7 == 0:
+        pass
+    elif number % 6 == 0:
+        pass
+    elif number % 5 == 0:
+        pass
+    elif number % 4 == 0:
+        pass
+    elif number % 3 == 0:
+        pass
+    elif number % 2 == 0:
+        pass
+    elif number % 1 == 0:
+        pass
 elif number % 9 == 0:
-    pass
+    if number % 8 == 0:
+        pass
+    elif number % 7 == 0:
+        pass
+    elif number % 6 == 0:
+        pass
+    elif number % 5 == 0:
+        pass
+    elif number % 4 == 0:
+        pass
+    elif number % 3 == 0:
+        pass
+    elif number % 2 == 0:
+        pass
+    elif number % 1 == 0:
+        pass
 elif number % 8 == 0:
     pass
 elif number % 7 == 0:
