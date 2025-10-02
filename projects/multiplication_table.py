f9a24a133f8aa318f077724198dfa1223c04fb82
abstudent133 <anna.bangerter@ucas-edu.net>
2025-10-02 09:23:15 -0600

    finish multiplication table

--- projects/multiplication_table.py
+++ projects/multiplication_table.py
@@ -1,7 +1,9 @@
 #AB 1st Multiplication Table
 
 nums = [1,2,3,4,5,6,7,8,9,10,11,12]
+print("This is a multiplication table with the numbers 1-12")
 
 for num in nums:
-    row = [num*1,num*2,num*3,num*4,num*5,num*6,num*7,num*8,num*9]
+    row = [num*1,num*2,num*3,num*4,num*5,num*6,num*7,num*8,num*9,num*10,num*11,num*12]
+    
     print(row)
