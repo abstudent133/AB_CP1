ba71060d0589065bed1e16743d18c18fd6931a7c
abstudent133 <anna.bangerter@ucas-edu.net>
2025-10-02 09:52:48 -0600

    worked on factoring project

--- fun_projects/factoring.py
+++ fun_projects/factoring.py
@@ -2,7 +2,7 @@ number = int(input("Give me a number you would like to factor:"))
 
 if number % 10 == 0:
     if number % 9 == 0:
-        pass
+       pass
     elif number % 8 == 0:
         pass
     elif number % 7 == 0:
@@ -37,17 +37,65 @@ elif number % 9 == 0:
     elif number % 1 == 0:
         pass
 elif number % 8 == 0:
-    pass
+    if number % 7 == 0:
+        pass
+    elif number % 6 == 0:
+        pass
+    elif number % 5 == 0:
+        pass
+    elif number % 4 == 0:
+        pass
+    elif number % 3 == 0:
+        pass
+    elif number % 2 == 0:
+        pass
+    elif number % 1 == 0:
+        pass
 elif number % 7 == 0:
-    pass
+    if number % 6 == 0:
+        pass
+    elif number % 5 == 0:
+        pass
+    elif number % 4 == 0:
+        pass
+    elif number % 3 == 0:
+        pass
+    elif number % 2 == 0:
+        pass
+    elif number % 1 == 0:
+        pass
 elif number % 6 == 0:
-    pass
+    if number % 5 == 0:
+        pass
+    elif number % 4 == 0:
+        pass
+    elif number % 3 == 0:
+        pass
+    elif number % 2 == 0:
+        pass
+    elif number % 1 == 0:
+        pass
 elif number % 5 == 0:
-    pass
+    if number % 4 == 0:
+        pass
+    elif number % 3 == 0:
+        pass
+    elif number % 2 == 0:
+        pass
+    elif number % 1 == 0:
+        pass
 elif number % 4 == 0:
-    pass
+    if number % 3 == 0:
+        pass
+    elif number % 2 == 0:
+        pass
+    elif number % 1 == 0:
+        pass
 elif number % 3 == 0:
-    pass
+    if number % 2 == 0:
+        pass
+    elif number % 1 == 0:
+        pass
 elif number % 2 == 0:
     pass
 elif number % 1 == 0:
