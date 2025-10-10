60c7c6c6bd38ef8d306e222889a4abc257372d2c
abstudent133 <anna.bangerter@ucas-edu.net>
2025-10-10 09:52:51 -0600

    password strength

---
+++ finals/password_strength.py
@@ -0,0 +1,16 @@
+#AB 1st password strength test
+
+#password is ask user for password
+#length is get length of password
+#length point is 0
+#uppercase point is 0
+#lowercase point is 0
+#numbers point is 0
+#special character point is 0
+#characters is password.index
+
+#if length is >= 8 then
+    #add 1 to the length point
+#for each character in password
+    #if character.islower
+        
---
+++ starters/starters/pseudocode.py
@@ -0,0 +1,19 @@
+#AB 1st Pseudocode
+
+    
+    if player == [1][4][7]:
+        print("You win!")
+        break
+    elif player == [2][5][8]
+        print("You win!")
+        break
+    elif player == [3][6][9]:
+        print("You win!")
+        break
+    elif computer == [1][2][3]:
+        print("You lose.")
+        break
+    elif computer == [4][5][6]:
+        print("You lose")
+        break
+    elif computer == [7][8][9]:
