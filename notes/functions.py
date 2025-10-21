c9db271c71b4f4ec7e98b315bd79f495c2c0c540
abstudent133 <anna.bangerter@ucas-edu.net>
2025-10-21 09:51:53 -0600

    ascii and function notes

--- notes/functions.py
+++ notes/functions.py
@@ -1,6 +1,17 @@
 #AB 1st function notes
 #set variables first then define functions
 
+player_health = 100
+monster_health = 100
+def damage(amount, turn):
+    if turn == "player":
+        return monster_health - amount, player_health
+    else:
+        return monster_health, player_health - amount
+monster_health, player_health = damage(10, "player")
+print(f"Monster Healthe: {monster_health}")
+print(f"Player Health: {player_health}")
+
 def add(x,y):
     #print(f"{x} + {y} = {x+y}")
     return x+y#this replaces
@@ -23,3 +34,6 @@ while x < add(3,9):
     x+=1
 print("Goose!")
 add(3,9)
+print(initials("Anna Bangerter"))
+print(f"a = {ord("a")}")
+print(f"98 = {chr(98)}")
