74e7f9b65577df0fc85720ec017d133f8ca1ae0e
abstudent133 <anna.bangerter@ucas-edu.net>
2025-10-23 03:28:12 +0000

    finish caesar cipher

--- projects/ceasar_cipher.py
+++ projects/ceasar_cipher.py
@@ -11,46 +11,51 @@
 message = ""
 
 #definition of action is(shift)
-def action(shift):
+def action(message, shift):
     new_message = ""
     for letter in message:
             #if letter is alpha
             if letter.isalpha():
-                #ord letter + shift
-                new_letter = chr(ord(letter) + shift)
-                #add new letter to message
-                message = new_message + new_letter
-                #return message
+                #if the letter is uppercase
+                if letter.isupper():
+                    #new letter is the ascii letter - 65 + the shift modulo 26 + 65
+                    new_letter = chr((ord(letter) - 65 + shift) % 26 + 65)
+                    #add new letter to new message
+                    new_message += new_letter
 
+                #else
+                else:
+                    #new letter is the ascii letter - 97 + the shift modulo 26 + 97 because it has to wrap around
+                    new_letter = chr((ord(letter) - 97 + shift) % 26 + 97)
+                    #add new letter to new message
+                    new_message += new_letter
+                
             #else
             else:
+                #add letter to new message
+                new_message += letter
                 #contiune
                 continue
-    return message
+    return new_message
     
 
 
 
 #Show Hello this is a Caesar cipher for encoding and decoding messages. If you would like to encode a message enter 1 and if you would like to decode enter 2.
 print("Hello. This is a Caesar cipher for encoding and decoding messages. If you would like to encode a message enter 1 but if you want to decode a message enter 2.")
-message = input("Please enter the code you would like to ")
+message = input("Please enter the code you would like to encode or decode: ")
 # choice is  ask Please enter the number of the action you would like to complete:
 choice = int(input("Please enter the number of the action you would like to complete: "))
 
 #if they chose 1 then
 if choice == 1:
-    #message is what is the message you would like to encode
-    message = input("What is the message you would like to encode: ")
-    
     #shift is ask choose an amount to shift the letters
     shift = int(input("What is the number the letters shifted by: "))
     #show message
-    print(f"This is the new message: {action(shift)}")
+    print(f"This is the new message: {action(message, shift)}")
 #or else they chose 2 then
 elif choice == 2:
-    #message is what is the message you would like to decode
-    message = str(input("What is the message you would like to decode: "))
     #shift is what is the amount that the message was shifted
     shift = int(input("What is the number the letters shifted by: "))
     #show message
-    print(f"This is the decoded message: {action(shift)}")
+    print(f"This is the decoded message: {action(message, -shift)}")
