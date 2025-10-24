da425efa28c414f5be317972d3412000587bff2f
abstudent133 <anna.bangerter@ucas-edu.net>
2025-10-24 09:52:43 -0600

    worked on combat project

---
+++ projects/combat.py
@@ -0,0 +1,31 @@
+#AB 1st Combat Program using Activity Diagram
+
+import random
+
+dice_1 = random.randint(1,20)
+
+knight = {
+    "health": 30,
+    "defense": 10,
+    "attack 1": dice_1 + 2,
+    "attack 2": 14,
+    "attack 3": monster["defense"] -= 2,
+
+}
+
+hunter = {
+    "health": 20,
+    "defense": 12,
+    "attack": dice_1 + 5
+}
+
+monster = {
+    "health": 25,
+    "defense": 10,
+    "attack": 12
+}
+
+fighter = [knight, hunter]
+choose_fighter = input("Please cho")
+def player(attack, defense):
+    fighter
