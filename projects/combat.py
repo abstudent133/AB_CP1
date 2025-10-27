532e232c00a6fa8603e5eae5b54594f1e81e5a03
abstudent133 <anna.bangerter@ucas-edu.net>
2025-10-27 18:18:28 +0000

    combat game

--- projects/combat.py
+++ projects/combat.py
@@ -5,6 +5,7 @@ import random
 dice_1 = random.randint(1,20)
 dice_2 = random.randint(1,8)
 coin = random.randint(1,2)
+turn = "none"
 
 health = 0
 defense = 0
@@ -19,12 +20,12 @@ monster_attack = dice_1 + 3
 def player(attack, monster_defense):
         monster_damage = attack - monster_defense
         monster_health -= monster_damage
-        return monster_damage
+        return monster_health
 
 def monster(monster_attack, defense):
         damage = monster_attack - defense
         health -= damage
-        return damage
+        return health
 
 print("This is a combat game. You are a fighter who has to defeat a monster. But first, you must choose what fighter you will be.")
 print("You must choose between a Wizard(1), a Knight(2), and a Rouge(3).")
@@ -57,6 +58,36 @@ else:
 print("You are being attacked by a Dark Gnome!")
 if coin == 1:
         print("You go first!")
-        action = input
+        turn = "player"
+else:
+        print("The monster goes first.")
+        turn = "monster"
+        
+        
+if turn == "player":
+        print("These are your combat options:")
+        print("1.Do attack")
+        print("2.Double attack but take damage")
+        print("3.Get 9 health")
+        print("4.Run away(you might not get away)")
+        action = input("Put the number of the action  you would like to complete: ")
+        if action == 1:
+                player(attack, monster_defense)
+                if attack > monster_defense:
+                        print("You hit!")
+                        print(f"This is the monster's health now: {monster_health}")
+                elif attack <= monster_defense:
+                        print("You missed.")
+        elif action == 2:
+                player(attack*2, monster_defense)
+                if attack > monster_defense:
+                        print("You hit!")
+                        print(f"This is the monster's health now: {monster_health}")
+                        print(f"This is your health now: {health - 3}")
+                elif attack <= monster_defense:
+                        print("You missed.")
+                        print(f"This ")
+
+                
     
 
