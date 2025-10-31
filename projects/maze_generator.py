1090577a3b66b0b6e1caae34fcf55e739b72be41
abstudent133 <anna.bangerter@ucas-edu.net>
2025-10-31 09:51:24 -0600

    start pseudocode for maze generator

---
+++ projects/maze_generator.py
@@ -0,0 +1,11 @@
+#AB 1st Maze Generator
+
+#import libraries(turtle and random)
+
+#set maze height to 400
+#set maze width to 400
+#set row to 20 squares
+
+#for each square in row
+    #line is random number between 1-4
+    
