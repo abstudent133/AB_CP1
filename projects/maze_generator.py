eefa962b19c01c4ba841b204443b544e5c86778e
abstudent133 <anna.bangerter@ucas-edu.net>
2025-11-03 09:51:40 -0700

    worked on maze generator

--- projects/maze_generator.py
+++ projects/maze_generator.py
@@ -2,10 +2,19 @@
 
 #import libraries(turtle and random)
 
-#set maze height to 400
-#set maze width to 400
-#set row to 20 squares
+#set maze height to 300
+#set maze width to 300
+#grid is coordinates of a 6 by 6 grid
+grid = [["row 1",(0,0),(1,0),(2,0),(3,0),(4,0),(5,0)],
+        ["row 2",(1,0),(1,1),(1,2),(1,3),(1,4),(1,5)],
+        ["row 3",(2,0),(2,1),(2,2),(2,3),(2,4),(2,5)],
+        ["row 4",(3,0),(3,1),(3,2),(3,3),(3,4),(3,5)]]
+#
 
-#for each square in row
-    #line is random number between 1-4
+
+
+#function of creating the lines on a column(col_coordinate, column)
+    #
+    #for each 
+
     
