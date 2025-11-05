038af87ff441f761b098c62c670569ae8945368e
abstudent133 <anna.bangerter@ucas-edu.net>
2025-11-05 09:53:14 -0700

    worked on maze generator

--- projects/maze_generator.py
+++ projects/maze_generator.py
@@ -5,40 +5,81 @@ import turtle as t
 import random as r
 
 cell_size = 50
-rows = 2
-columns = 2
-
-top = []
-bottom = []
-left = []
-right = []
+rows = 6
+columns = 6
+boxes = [[],[],[],[]]
 visited = []
+row_grid = []
+column_grid = []
+
+
+
 
 
 
 #set up turtle
 def setup_turtle():
-    t.setup(columns * cell_size + 50, rows * cell_size + 50)
     t.title("Maze")
     t.hideturtle()
     t.speed(0)
     t.pensize(3)
     t.penup()
 def grid_setup():
-    for i in range(rows*columns):
-        top.append(1)
-        bottom.append(1)
-        left.append(1)
-        right.append(1)
-        visited.append(0)
+    for i in range(1, 36):
+        boxes[0].append(1)
+        boxes[1].append(1)
+        boxes[2].append(1)
+        boxes[3].append(1)
 
-def draw_grid():
-    t.setpos(0,columns * cell_size)
+def randomize_walls():
+    grid_setup()
+    for n in boxes[0]:
+        choice = r.randint(0,1)
+        if choice == 0:
+            n = 0
+        else:
+            continue
+    for n in boxes[1]:
+        choice = r.randint(0,1)
+        if choice == 0:
+            n = 0
+        else:
+            continue
+    for n in boxes[2]:
+        choice = r.randint(0,1)
+        if choice == 0:
+            n = 0
+        else:
+            continue
+    for n in boxes[3]:
+        choice = r.randint(0,1)
+        if choice == 0:
+            n = 0
+        else:
+            continue
+    return boxes
     
-def main():
-    setup_turtle()
-    t.done()
-main()   
+        
+        
+        
+def draw_grid():
+    t.setpos(0,0)
+    for i in range(rows +1):
+        t.pendown()
+        for l in range(1,5):
+            t.forward(50)
+            t.left(90)
+        
+
+    
+
+setup_turtle()
+grid_setup()
+print(grid_setup())
+randomize_walls()
+print(randomize_walls())
+t.done()
+
 
     
 
