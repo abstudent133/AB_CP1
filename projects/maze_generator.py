ee0bef8cbd455f1e15f1416f6acf28d9a30f456a
abstudent133 <anna.bangerter@ucas-edu.net>
2025-11-05 04:27:59 +0000

    worked on maze generator

--- projects/maze_generator.py
+++ projects/maze_generator.py
@@ -4,47 +4,41 @@
 import turtle as t
 import random as r
 
-#set maze height to 300
-height = 300
-#set maze width to 300
-width = 300
-#number of boxes
-boxes = 36
-#row grid
-row_grid = [[(0,0),(1,0),(2,0),(3,0),(4,0),(5,0)],
-        [(1,0),(1,1),(1,2),(1,3),(1,4),(1,5)],
-        [(2,0),(2,1),(2,2),(2,3),(2,4),(2,5)],
-        [(3,0),(3,1),(3,2),(3,3),(3,4),(3,5)],
-        [(4,0),(4,1),(4,2),(4,3),(4,4),(4,5)],
-        [(5,0),(5,1),(5,2),(5,3),(5,4),(5,5)]]
-#create a grid that shows if there is a line or not called grid_row
-grid_row = [[0,0,0,0,0,0],
-            [0,0,0,0,0,0],
-            [0,0,0,0,0,0],
-            [0,0,0,0,0,0],
-            [0,0,0,0,0,0],
-            [0,0,0,0,0,0],]
-#column grid
-col_grid = [[(0,0),(1,0),(2,0),(3,0),(4,0),(5,0)],
-        [(1,0),(1,1),(1,2),(1,3),(1,4),(1,5)],
-        [(2,0),(2,1),(2,2),(2,3),(2,4),(2,5)],
-        [(3,0),(3,1),(3,2),(3,3),(3,4),(3,5)],
-        [(4,0),(4,1),(4,2),(4,3),(4,4),(4,5)],
-        [(5,0),(5,1),(5,2),(5,3),(5,4),(5,5)]]
-#create a grid to show if ther is a line or not in the colums called grid_col
-grid_col = [[0,0,0,0,0,0],
-            [0,0,0,0,0,0],
-            [0,0,0,0,0,0],
-            [0,0,0,0,0,0],
-            [0,0,0,0,0,0],
-            [0,0,0,0,0,0],]
-making_maze = True
-while making_maze:
-        for num in grid_row:
-                change_num = r.randint(0,1)
-                num_index = num.index(num)
-                grid_row[num_index] = change_num
-        
+cell_size = 50
+rows = 2
+columns = 2
+
+top = []
+bottom = []
+left = []
+right = []
+visited = []
+
+
+
+#set up turtle
+def setup_turtle():
+    t.setup(columns * cell_size + 50, rows * cell_size + 50)
+    t.title("Maze")
+    t.hideturtle()
+    t.speed(0)
+    t.pensize(3)
+    t.penup()
+def grid_setup():
+    for i in range(rows*columns):
+        top.append(1)
+        bottom.append(1)
+        left.append(1)
+        right.append(1)
+        visited.append(0)
+
+def draw_grid():
+    t.setpos(0,columns * cell_size)
+    
+def main():
+    setup_turtle()
+    t.done()
+main()   
 
     
 
