fb09c0d9574b32ce859281b6d36fc86318d05acb
abstudent133 <anna.bangerter@ucas-edu.net>
2025-11-11 09:52:54 -0700

    finished psuedocode for order up

--- projects/order_up.py
+++ projects/order_up.py
@@ -7,12 +7,39 @@
 #make order dictionary
     #drink
     #main course
-    #side dish dictionary
+    #side dish 1
+    #side dish 2
+#definition check(ordering)
+    #if ordering in key
+        #return true
+    #else
+        #reuturn false
 
-#definition take order(ordering)
-    #if ordering is drinks
-        
+#definition take order(ordering, key)
+    #call check(ordering)
+    #if check(ordering) is true
+        #order [key] = ordering
+        #return ordering
+#definition get price(ordering, key)
+    #price is menu [key][ordering]
+    #return price
 
 #print the menu
 
+#for key in order
+    # if key is drink
+        #ordering is ask which drink they want
+    #elif key is main course
+        #ordering is ask which main course they want
+    #elif key is side dish 1
+        #ording is ask which side dish they want first
+    #elif key is side dish 2
+        #ordering is ask which side dish they want second
+    #call take order(ordering, key)
+#for key in order
+    #price is get price(ordering, key)
+    #total += price
+#for key in order
+    #print key
+#show total
 
