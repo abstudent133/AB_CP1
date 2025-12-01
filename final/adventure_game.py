43a9c509d2bbdf110513da9e0a3a8874374ba1e1
abstudent133 <anna.bangerter@ucas-edu.net>
2025-12-01 09:51:38 -0700

    worked on psuedocode for adventure game

---
+++ final/adventure_game.py
@@ -0,0 +1,51 @@
+#AB 1st Text Based Adventure Game
+#import random
+
+#Dictionaries
+#Player ditionary
+#player = {
+#   health: 30,
+#   stealth: 5,
+#   strength: 20,
+#   attack: 5,
+#   agility: 10
+#   items: []}
+#Doof dictionary
+#doof = {
+#   health: 40,
+#   strength: 20,
+#   agility: 10,
+#   attack: 7,
+#   inator: {
+#       health: 30,
+#       strength: 15}
+#   }
+#Norm dictionary
+#norm = {
+#   health: 20
+#   strength: 15
+#   agility: 5
+#   attack: 5}
+
+#Variables
+#watch = 2
+#
+
+#Room 1 funtion
+#Room 2 funtion
+#Room 3 funtion
+#Room 4 funtion
+#Room 5 funtion
+#Room 6 funtion
+#Room 7 funtion
+#Room 8 funtion
+#Room 9 funtion
+#Room choice funtion
+#Health change funtion
+#Attack funtion
+#Play again funiton
+#Main Loop
+#while true
+#   describe mission to player
+#   tell them they are in the Fletchers backyard and they have to sneak away without being caught by the Fletchers
+#   while true
