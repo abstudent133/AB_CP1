00d1431f0e273d189ff14b865257d601d33395cc
abstudent133 <anna.bangerter@ucas-edu.net>
2025-12-03 09:53:11 -0700

    worked on psuedocode

--- final/adventure_game.py
+++ final/adventure_game.py
@@ -1,35 +1,48 @@
 #AB 1st Text Based Adventure Game
 #import random
 
-#Dictionaries
+#DICTIONARIES
 #Player ditionary
-#player has perry's health, stealth, and basic attack
+#player has perry's health, stealth, agility,strength, and basic attack
 #Doof dictionary
 #doof has Doofenshmirz's health, attack, and inator stats
 #   }
 #Norm dictionary
-#norm = {
-#   health: 20
-#   attack: 5}
+#norm has norm's health and basic attack
 
 #Variables
-#watch = 2
-#
+#gadgets is a list of gadgets perry has attained
+#visited rooms is a list of the rooms that were visited
 
-#Room 1 funtion
-#Room 2 funtion
-#Room 3 funtion
-#Room 4 funtion
-#Room 5 funtion
-#Room 6 funtion
-#Room 7 funtion
-#Room 8 funtion
-#Room 9 funtion
+#FUNCTIONS
+#Room 1 funtion(Fletcher Yard)
+#For this function the parameters is the gadgets list, kwarg of perry's stats(health,stealth,strength), visited rooms list
+#   first the computer needs to check the visited rooms list
+#   if room 1 was on that list
+#       if money was on the gadgets list or perry's stealth is 0
+#           return there is nothing for you in this room choose another
+#       else money was not on the gadgets list and perry's health is 1 or greater
+#           choice is the choice to steal the money(1) or go to another room(2)
+#           if choice is 1
+#               append gadgets list with money
+#               return gadgets list and a message saying you have the money
+#           else choice is 2
+#               return message about going to another room
+#   if money was found on
+#Room 2 funtion(Danvil Park)
+#Room 3 funtion(City Hall)
+#Room 4 funtion(Subway Tunnels)
+#Room 5 funtion(Slushy Burger)
+#Room 6 funtion(Waste Recycling Plant)
+#Room 7 funtion(Water Tower)
+#Room 8 funtion(Alleyway Hideout)
+#Room 9 funtion(Doofenshmirz Evil Inc.)
 #Room choice funtion
 #Health change funtion
 #Attack funtion
 #Play again funiton
-#Main Loop
+
+#MAIN LOOP
 #while true
 #   describe mission to player
 #   tell them they are in the Fletchers backyard and they have to sneak away without being caught by the Fletchers
