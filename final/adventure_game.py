fcfe79b55cfa597756ddd6f7a9b99bf26610adb7
abstudent133 <anna.bangerter@ucas-edu.net>
2025-12-05 15:53:38 +0000

    worked on psuedocode

--- final/adventure_game.py
+++ final/adventure_game.py
@@ -15,18 +15,19 @@
 #visited rooms is a list of the rooms that were visited
 
 #FUNCTIONS
+#Note: any time I describe checking perry's stats, the gadget list, or the visited rooms plan on creating nested conditionals
 #Room 1 funtion(Fletcher Yard)
 #The goal of this funtion is to get money from the Fletchers and gain some stealth or strength
-#For this function the parameters is the gadgets list, kwarg of perry's stats(health,stealth,strength), visited rooms list
-#   first the computer needs to check the visited rooms list with the list checker funtion
+#For this function the parameters is the gadgets list, kwarg of perry's stats(health,stealth,strength), visited rooms 
+#   first the computer needs to check true or false if the room has been visited
 #   Then the computer need to check if the gadget is in list with the list checker funtion
 #   Then the computer has to check if the player has the correct stats based on what is necessary
 #   Use conditionals about if the room has been visited, if you have money already, and if you have the correct stats to steal the money if you didn't already to get an outcome
 
 #Room 2 funtion(Danvil Park)
 #The goal of this funtion is to get the keycard for Doofenshmirz Evil Inc. and you have to fight norm for it unless you have the correct number of stats
-#For this funtion the parameters are perry's stats, the gadget list, and the room visited list
-#   First use the visited rooms funtion to check if the player has been there before
+#For this funtion the parameters are perry's stats, the gadget list, and the room visited 
+#   First use the true or false if the room has been visited
 #   Then use the gadget checker funtion to check if the player already has the gadget
 #   If they haven't gotten the object then they need to fight norm bot so I would call the combat funtion
 #   Then the computer has to check if the player has the correct stats based on what is necessary
@@ -34,7 +35,8 @@
 
 #Room 3 funtion(City Hall)
 #The goal of this funtion is to gain a signal jammer and you need to warn Mayor Roger Doofenshmirz but to do that you need the platypus translator also you can gain some agility
-#For this funtion the parameters are the gadget list and perry's stats
+#For this funtion the parameters are the gadget list and perry's stats and true or false if the room has been visited
+#   First check if the room has been visited
 #   First check the gadget list to see if you have the signal jammer if so then you just exit the room
 #   If you haven't then the computer uses the list checker again to check if the player has the platypus translator then you can warn the mayor and get the signal jammer other wise you have to leave
 #   If you haven't already been there then you can gain an agility point
@@ -42,11 +44,22 @@
 #Room 4 funtion(Subway Tunnels)
 #This room is the only way to get to the final boss but you need to have a flashlight
 #For this funtion the parameters are the gadget list
-#   If the flashlight is on the gadget list(checked with the list checker funtion) then the player can enter other wise they can't
+#   If the flashlight is on the gadget list(checked with the list checker funtion) then the player can enter other wise they can't enter
+#   If they do have the flashlight then they can enter and choose to leave go to Doofenshmirtz Evil Inc. or to the Alley way hide out
 
 #Room 5 funtion(Slushy Burger)
-#The goal of this room is to gain health from a burger but you need to have money
+#The goal of this room is to gain health from a burger but you need to have money also you can take a flash light if you have the correct stealth level
+#For this funtion the parameters are perry's health, the gadget list, the visited rooms true or false
+#   First if the room has been visited true or false
+#   Depending on that answer you can use money if you have it to gain the health or you just have to leave or you want to try and take a flashlight
+#   If they want to take the flash light then they have to have at least level 1 stealth otherwise they would lose health and not get the flashlight
+#   The result would be perry's health and the gadget list 
+
 #Room 6 funtion(Waste Recycling Plant)
+#In this room Doof has set up a machine that broadcasts his mind controling waves farther and you have to jam it with the signal jammer but you have to have a certain level of agility to do it. This weakens the inator making it easier to break
+#For this funtion the parameters are the gadget list, perry's agility, and the visited rooms 
+#
+
 #Room 7 funtion(Water Tower)
 #Room 8 funtion(Alleyway Hideout)
 #Room 9 funtion(Doofenshmirz Evil Inc.)
